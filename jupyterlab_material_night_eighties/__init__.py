25692d6837017f87dc46fc2f900dfd50785b8de2
Alex Graber <alex.graber@pmi.org>
2022-03-03 11:45:37 -0500

    fix from naive cookiecutter

--- jupyterlab_material_night_eighties/__init__.py
+++ jupyterlab_material_night_eighties/__init__.py
@@ -1,16 +1,19 @@
 import json
-import os
+from pathlib import Path
 
 from ._version import __version__
 
-HERE = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(HERE, 'labextension', 'package.json')) as fid:
+HERE = Path(__file__).parent.resolve()
+
+
+with (HERE / "labextension" / "package.json").open() as fid:
     data = json.load(fid)
 
 
 def _jupyter_labextension_paths():
     return [{
-        'src': 'labextension',
-        'dest': data['name']
+        "src": "labextension",
+        "dest": data["name"]
     }]
+
